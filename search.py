7e4d71a33cd4a51ab1847d6af6cf595320269ee2
Mateus Assis <mateus.d.assis.silva@gmail.com>
2026-02-06 22:29:27 -0300

    feat: check before walking

--- search.py
+++ search.py
@@ -51,27 +51,58 @@ class Search:
         """
         return self.wolf_link.position in {self.statue_shadow.position, self.statue_mirror.position}
     
-    def move(self, new_link_position: int):
-        """Move Wolf Link to a new tile and update the statues' positions according to their patterns.
+    def will_link_move_to_invalid_position(self, new_link_position: int, new_shadow_statue_position: int, new_mirror_statue_position: int) -> bool:
+        """Check if Wolf Link will move to a tile occupied by a statue after the move.
+
+        Args:
+            new_link_position (int): the tile number where Wolf Link will move to.
+            new_shadow_statue_position (int): the tile number where the shadow statue will move to.
+            new_mirror_statue_position (int): the tile number where the mirror statue will move to.
+        Returns:
+            bool: true if Wolf Link will be on the same tile as a statue after the move
         """
-        link_movement_direction = self.graph.get_edge_data(self.wolf_link.position, new_link_position).get('dir')
+        return new_link_position in {new_shadow_statue_position, new_mirror_statue_position}
+    
+    def get_shadow_statue_new_position(self, link_movement_direction: str) -> int:
+        """Calculate the new position of the shadow statue based on Wolf Link's movement direction.
 
-        # Mirror statue moves to the tile opposite to Wolf Link's new position
-        mirror_statue_direction = self.opposite_directions[link_movement_direction]
-        mirror_statue_new_position = next(
-            iter(
-                [n for n in self.graph.neighbors(self.statue_mirror.position) if self.graph.get_edge_data(self.statue_mirror.position, n).get('dir') == mirror_statue_direction]
-            ), self.statue_mirror.position
-        ) # If no valid move for the mirror statue, it stays in place
-        self.statue_mirror.move(mirror_statue_new_position)
+        Args:
+            link_movement_direction (str): the direction in which Wolf Link is moving ('N', 'S', 'E', 'W').
 
-        # Shadow statue moves to the tile in the same direction as Wolf Link's new position
+        Returns:
+            int: the new tile number for the shadow statue, or its current position if it cannot move.
+        """
         shadow_statue_new_position = next(
             iter(
                 [n for n in self.graph.neighbors(self.statue_shadow.position) if self.graph.get_edge_data(self.statue_shadow.position, n).get('dir') == link_movement_direction]
             ),
             self.statue_shadow.position # If no valid move for the shadow statue, it stays in place
         )
+        return shadow_statue_new_position
+    
+    def get_mirror_statue_new_position(self, link_movement_direction: str) -> int:
+        """Calculate the new position of the mirror statue based on Wolf Link's movement direction.
+
+        Args:
+            link_movement_direction (str): the direction in which Wolf Link is moving ('N', 'S', 'E', 'W').
+
+        Returns:
+            int: the new tile number for the mirror statue, or its current position if it cannot move.
+        """
+        mirror_statue_direction = self.opposite_directions[link_movement_direction]
+        mirror_statue_new_position = next(
+            iter(
+                [n for n in self.graph.neighbors(self.statue_mirror.position) if self.graph.get_edge_data(self.statue_mirror.position, n).get('dir') == mirror_statue_direction]
+            ), 
+            self.statue_mirror.position
+        ) # If no valid move for the mirror statue, it stays in place
+        return mirror_statue_new_position
+
+    def move(self, new_link_position: int, mirror_statue_new_position: int, shadow_statue_new_position: int):
+        """Move Wolf Link to a new tile and update the statues' positions according to their patterns.
+        """
+        # Move Statues to new positions 
+        self.statue_mirror.move(mirror_statue_new_position)
         self.statue_shadow.move(shadow_statue_new_position)
 
         # Move Wolf Link to the new position
@@ -87,17 +118,34 @@ class Search:
                 print("No more available moves for Wolf Link. Search failed.")
                 break
             else:
-                # choose randomly where to move
-                next_link_position = random.choice(list(where_can_link_go))
-                # go there
-                self.move(next_link_position)
-                # increase steps
-                steps += 1
+                # shuffle next possible tiles for link:
+                shuffled_next_tiles = list(where_can_link_go)
+                random.shuffle(shuffled_next_tiles)
+
+                # Now, for each one, try if it is safe:
+                for next_link_position in shuffled_next_tiles:
+                    print(next_link_position)
+                    link_movement_direction = self.graph.get_edge_data(self.wolf_link.position, next_link_position).get('dir')
+                    shadow_statue_new_position = self.get_shadow_statue_new_position(link_movement_direction)
+                    mirror_statue_new_position = self.get_mirror_statue_new_position(link_movement_direction)
+
+                    print(f"Trying to move Wolf Link to {next_link_position} with direction {link_movement_direction}. Shadow Statue would move to {shadow_statue_new_position}, Mirror Statue would move to {mirror_statue_new_position}.")
+                    if self.will_link_move_to_invalid_position(next_link_position, shadow_statue_new_position, mirror_statue_new_position):
+                        print("Wolf Link moved to an invalid position. Search failed.")
+                    else:
+                        # go there
+                        self.move(
+                            next_link_position,
+                            mirror_statue_new_position,
+                            shadow_statue_new_position
+                        )
+                        # increase steps
+                        steps += 1
+                        break
             if self.has_reached_goal():
                 print("Goal reached!")
                 break
             if self.has_link_moved_to_invalid_position():
-                print("Wolf Link moved to an invalid position. Search failed.")
                 break
             if steps >= self.maximum_steps:
                 print("Maximum steps reached. Search failed.")
